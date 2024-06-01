9d66bddad3bd12d947b9df65e25250efd3a0a148
spacescape <15368104568@189.cn>
2024-06-01 19:27:28 +0800

    Update t1234567.c

--- tt/t1234567.c
+++ tt/t1234567.c
@@ -1,8 +1,3 @@
-#include "../Header/tzyx.h"
-#include "../Header/xyzt.h"
-#include "../Header/ijk.h"
-#include "../Header/lmn.h"
-#include "../Header/opq.h"
 #include "tt.c"
 #define ijk [t1][t2]
 #define lmn [t3][t4][t5]
