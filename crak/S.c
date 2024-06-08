a1f33485c7f0e5237b7c6b4716a75c3d7eac3cd4
ace <15368104568@189.cn>
2024-06-08 12:27:04 +0800

    Update S.c

--- crak/S.c
+++ crak/S.c
@@ -1,9 +1,4 @@
 #if S
-#include "S.ts"
-#include "T.c"
-#include "ijk.h"
-#include "lmn.h"
-#include "opq.h"
 #define k  0.000000000000000013806485279 J/k
 #define h 0.00000 00000 00000 00000 00000 6626 07015 J*s
 #define c 277792458 m/s
