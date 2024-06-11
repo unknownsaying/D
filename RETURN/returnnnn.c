87d33376b7e5d26c2bfd0061a0ff6bbe0fba7533
ace <15368104568@189.cn>
2024-06-11 21:08:24 +0800

    Update returnnnn.c

--- RETURN/returnnnn.c
+++ RETURN/returnnnn.c
@@ -4,27 +4,27 @@
 void main(){
     char ww;
     double asin , acos;
-    float t3, t4;
+    float t5, t6;
 }
-struct cube t3()t4{
-    struct cube t3()
-    struct cube t4()
+struct cube t5()t6{
+    struct cube t5()
+    struct cube t6()
 }
 void fh() {
-    return e % asinh(ww + t3) + g % acosh(ww - t4)
-    return g % asinh(ww - t4) + e % acosh(ww + t3)
+    return e % asinh(ww + t5) + g % acosh(ww - t6)
+    return g % asinh(ww - t6) + e % acosh(ww + t5)
     function returnnnn() {
         void (RET-URN)
-        return e % acosh(ww + t3) + g % acosh(ww - t4)
-        return g % asinh(Ww - t4) + e % asinh(ww + t3)
+        return e % acosh(ww + t5) + g % acosh(ww - t6)
+        return g % asinh(Ww - t6) + e % asinh(ww + t5)
         void (RTR-EUN)
-        return e % acosh(ww - t3) + g % acosh(ww + t4)
-        return e % asinh(wW + t4) + g % asinh(ww - t3)
+        return e % acosh(ww - t5) + g % acosh(ww + t6)
+        return e % asinh(wW + t6) + g % asinh(ww - t5)
         void (RUN-ETR)
-        return e % asinh(ww + t4) + g % asinh(ww - t3)
-        return g % acosh(ww + t3) + e % acosh(ww - t4)
+        return e % asinh(ww + t6) + g % asinh(ww - t5)
+        return g % acosh(ww + t5) + e % acosh(ww - t6)
         void (REN-TUR)
-        return e % asinh(ww - t3) + g % asinh(ww + t4)
-        return g % acosh(ww - t4) + e % acosh(ww + t3)
+        return e % asinh(ww - t5) + g % asinh(ww + t6)
+        return g % acosh(ww - t6) + e % acosh(ww + t5)
     }
 }
