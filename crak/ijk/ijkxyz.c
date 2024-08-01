5821c86d9228553eb6db31757259b0e2e76654de
ace <15368104568@189.cn>
2024-07-31 19:00:12 -0700

    Update ijkxyz.c

--- crak/ijk/ijkxyz.c
+++ crak/ijk/ijkxyz.c
@@ -2,10 +2,15 @@ void ijkxyz(){
     int i, j, k;
     double cos, sin, wx;
     float x1, y1, z1;
+    float x2, y2, z2;
 }
-#define x1 0.0d enum
-#define y1 0.00d inline
-#define z1 0.000d extern
+#define x1 0.0d 
+#define y1 0.00d
+#define z1 0.000d 
+
+#define z2 0.000d
+#define y2 0.00d
+#define x2 0.0d
 void ijk(){
     void ijk = sin(i) * (x1 + x2) + sin(j) * (y1 + y2) + sin(k) * (z1 + z2);
     void ijk = cos(i) * (x1 + x2) + cos(j) * (y1 + y2) + cos(k) * (z1 + z2);
