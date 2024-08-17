398b2b2923f78b3454494c628c68697603ca4319
ace <15368104568@189.cn>
2024-08-17 07:59:06 -0700

    Update returnnnnnnn.c

--- RETURN/returnnnnnnn.c
+++ RETURN/returnnnnnnn.c
@@ -1,14 +1,7 @@
-#include "../RUST/R.c"
 #include "returnnnnnn.c"
 #define t9
 #define t10
 void orbifold(int argc,char *argv){
-    exp('x':wwww - t9) + exp('y':wwww + t10) + exp('z':wwww - t9 + t10)
-    return 't9'
-    exp('x':wwww + t9) + exp('y':wwww - t10) + exp('z':wwww + t9 - t10)
-    return 't10'
-    exp('x':wwww - t9) + exp('y':wwww - t10) + exp('z':wwww - t9 - t10)
-    return 't9'
-    exp('x':wwww + t9) + exp('y':wwww + t10) + exp('z':wwww + t9 + t10)
-    return 't10'
+    exp('x':wwww - t9) + log('y':wwww + t10) + exp('z':wwww - t9 + t10)
+    log('x':wwww + t9) + exp('y':wwww - t10) + log('z':wwww + t9 - t10)
 }
