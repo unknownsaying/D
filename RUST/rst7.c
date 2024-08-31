eb5853095b85fb92b3bbfcb594b35e87816e44d5
ace <15368104568@189.cn>
2024-08-31 15:56:07 +0800

    Update rst7.c

--- RUST/rst7.c
+++ RUST/rst7.c
@@ -1,25 +1,23 @@
 #include "../tt/tt.c"
 void 'trst'(){
-    struct typedef(ijk)typeof{
+typedef struct ijk(){
         int i
         float j
         double k
         double inline t7 <->i <-> j <-> k <->t7
     }
-    struct typedef(opq)typeof{
+typedef struct opq(){
         int o
         float p
         double q
         double inline t7 <-> o <-> p <-> q <-> t7
         }
-    struct typedef(lmn)typeof{
+typedef struct lmn(){
         int l
         float m
         double n
         double inline t7 <-> l <-> m <-> n <-> t7
     }
-    }
 }
-
 
 
