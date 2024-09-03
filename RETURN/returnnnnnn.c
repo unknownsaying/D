06e7e8f3370308206a071953e39607bac974f7aa
ace <15368104568@189.cn>
2024-09-03 13:54:34 +0800

    Update returnnnnnn.c

--- RETURN/returnnnnnn.c
+++ RETURN/returnnnnnn.c
@@ -4,21 +4,6 @@
 #define t7
 #define t8
 void manifold(){
-      e % (returnnnn + t7) + g % (returnnnnn + t8)
-      g % (returnnnnn - t8) + e % (returnnnn + t7)  
     return g % ldexp(wx + e) + e % ldexp(ww - g)
     return e % frexp(ww - g) + g % frexp(wx + e)
-     }
-void inline(){
-    exp(ww + e) 
-    exp(wx - g)
-    return returnnnn
-    log(ww + wx * e) 
-    log(wx * e + ww)
-    return returnnnnn
-    }
-void extern(){
-    exp(w * e - float * 'function') + log(w * e - 'float' * 'functionc'  )
-    log(w % e - float * 'functionc' ) + exp(w % e - 'float' *'function' )
-    }
 }
