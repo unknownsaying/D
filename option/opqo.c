513e5c5f6b9e8e462a3f1051af83df1e2d0f1b57
ace <15368104568@189.cn>
2024-11-07 19:29:04 +0800

    Update opqo.c

--- option/opqo.c
+++ option/opqo.c
@@ -1,25 +1,24 @@
 #if opq
 void main(){
-double o ,q ,p ;
-unsigned long int sh, ch ,www;
+    double o ,q ,p ;
+    unsigned long int sh, ch ,www;
 }
 int t5{
-        function t5(){
-            {
-                int16_t
-                sh(www + o)
-                int32_t
-                ch(www - p)
-                int64_t
-                sh(www + q)
-            }}
-        int t6{
-            function t6(){
-                int512_t
-                ch(www - p)
-                int256_t
-                sh(www - q)
-                int128_t
-                ch(www + o)
-            }}}
+   function t5(){
+    int16_t
+    sh(www + o)
+    int32_t
+    ch(www - p)
+    int64_t
+    sh(www + q)
+}}
+int t6{
+   function t6(){
+    int512_t
+    ch(www - p)
+    int256_t
+    sh(www - q)
+    int128_t
+    ch(www + o)
+}}
 #endif
