72496812dc428df2ea9b20f12d22242b1bc18471
ace <15368104568@189.cn>
2024-11-07 19:13:13 +0800

    Update N1inventorySlots.c

--- InventorySlot/N1inventorySlots.c
+++ InventorySlot/N1inventorySlots.c
@@ -11,16 +11,17 @@ for (int k = 0; k < t6t7; ++k) {
 }
 for (int k = 0; k < t7t6; k++){
 }
-void N1() {
-    unsigned long int t1t2 = t1 / t2
-    unsigned long int t3t4 = t3 / t4
-    unsigned long int t6t7 = t6 / t7
-    for (float l; l < t1t2; ++i) {
+void N1(){
+    unsigned long int t8 = t1 / t2
+    unsigned long int t9 = t3 / t4
+    unsigned long int t10 = t6 / t7
+    
+    for (float l; l < t1t2; ++l) {
     }
-    for (float m; m > t4t5; ++t) {
-        {
+    for (float m; m > t4t5; m--) {
+    }
     for (float n; n < t6t7; ++n) {
-}        
+    }        
     for (flaot n; n > t7t6; n++) {
     }
 }
