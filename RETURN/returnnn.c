73422a689edbe21831c7ab0ebe5dd87450bebe14
ace <15368104568@189.cn>
2024-11-07 19:20:14 +0800

    Update returnnn.c

--- RETURN/returnnn.c
+++ RETURN/returnnn.c
@@ -2,15 +2,15 @@
 void main(){
     char w;
     double asin , acos;
-    float t3, t4;
+        float t3, t4;
 }
 struct rect t3t4(){
     struct rect t3();
     struct rect t4();
 }
-void function returnnn() {
-        return h / acos(w + t3) + f / acos(w - t4)
-        return f / asin(w - t4) + h / asin(w + t3)
-        return h / acos(w - t4) + f / acos(w + t3)
+void returnnn(){
         return f / asin(w + t3) + h / asin(w - t4)
-    }
+        return h / acos(w + t3) + f / acos(w - t4)
+        return f / asin(w + t3) + h / asin(w + t4)
+        return h / acos(w - t3) + f / acos(w - t4)
+}
