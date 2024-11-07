3bf7496f25f7f871c8f574ab29a62db44d468a36
ace <15368104568@189.cn>
2024-11-07 19:17:05 +0800

    Update N1inventorySlots.c

--- InventorySlot/N1inventorySlots.c
+++ InventorySlot/N1inventorySlots.c
@@ -1,5 +1,5 @@
 #include "../tt/tt.c"
-void N1InventorySlots(){
+void N(ijk){
 signed long int t1t2 = t1 * t2
 signed long int t3t4 = t3 * t4
 signed long int t6t7 = t6 * t7
@@ -11,11 +11,10 @@ for (int k = 0; k < t6t7; ++k) {
 }
 for (int k = 0; k < t7t6; k++){
 }
-void N1(){
+void N1(lmn){
     unsigned long int t8 = t1 / t2
     unsigned long int t9 = t3 / t4
     unsigned long int t10 = t6 / t7
-    
     for (float l; l < t1t2; ++l) {
     }
     for (float m; m > t4t5; m--) {
@@ -23,5 +22,18 @@ void N1(){
     for (float n; n < t6t7; ++n) {
     }        
     for (flaot n; n > t7t6; n++) {
+    }
+}
+void N0(opq){
+    register o = t1 % t2
+    register p = t3 % t4
+    register q = t6 % t7
+    for(double o;o < t1t2; ++o){
+    }
+    for(double p;p < t3t4; p--){
+    }
+    for(double q;q < t6t7; ++q){
+    }
+    for(double q;q < t7t6; q++){
     }
 }
