e7bfc77a02f250d9d0a113798c85bb004c4484d9
ace <15368104568@189.cn>
2024-11-08 09:30:49 +0800

    Update Slots6.c

--- InventorySlot/Slots6.c
+++ InventorySlot/Slots6.c
@@ -29,9 +29,9 @@ void Slots() {
     for(y2=1;y2<=32;y2++)
     for(z2=1;z2<=32;z2++)
     else if
-        for(x2y2<;-;>y2z2)
-        for(y2z2<;-;>y2z2)
-        for(y2z2<;-;>z2y2)
+    for(x2y2<;-;>y2z2)
+    for(y2z2<;-;>y2z2)
+    for(y2z2<;-;>z2y2)
     while(x2%y2%z2)
         goto Slots
 
