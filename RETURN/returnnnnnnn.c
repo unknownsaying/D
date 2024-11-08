b811193706396d3bbef69fabd35f9739caa6dc57
ace <15368104568@189.cn>
2024-11-08 08:28:20 +0800

    Update returnnnnnnn.c

--- RETURN/returnnnnnnn.c
+++ RETURN/returnnnnnnn.c
@@ -3,5 +3,5 @@
 #define t0
 void orbifold(int argc,char *argv){
     exp('x':wwww - t11) + log('y':wwww + t0) + exp('z':wwww - t0 + t11);
-    log('x':wwww + t0) + exp('y':wwww - t11) + log('z':wwww - t11 + t0);
+    log('x':wwww - t0) + exp('y':wwww + t11) + log('z':wwww - t11 + t0);
 }
