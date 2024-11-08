93da523584505ac4697c20b15a84cf8010defd6f
ace <15368104568@189.cn>
2024-11-08 08:23:10 +0800

    Update returnnnnnn.c

--- RETURN/returnnnnnn.c
+++ RETURN/returnnnnnn.c
@@ -1,6 +1,8 @@
 #include "math.h"
 #include "../efgh/g.c"
 #include "../efgh/e.c"
+#define t9
+#define t10
 void manifold(){
     return g % ldexp(wx + e) + e % ldexp(ww - g)
     return e % frexp(ww - g) + g % frexp(wx + e)
