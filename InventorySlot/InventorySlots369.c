f61bbf6d0f65d8938bc30594c0c20ff07fd0d1c9
ace <15368104568@189.cn>
2024-11-08 08:36:17 +0800

    Update InventorySlots369.c

--- InventorySlot/InventorySlots369.c
+++ InventorySlot/InventorySlots369.c
@@ -93,11 +93,11 @@ void InventorySlots(){
        goto InventorySlots
 }
 void InventorySlots() {
-    unsigned long long int t6, t7, t8, t9;
-    unsigned long long int t6 = ("ABCD||DCBA")
-    unsigned long long int t7 = ("ABAB\\//CDCD")
-    unsigned long long int t8 = ("AB|/AB/|CD|/DC")
-    unsigned long long int t9 = ("CD&&DC\\//AB&&BA")
+    signed long long int t6, t7, t8, t9;
+    signed long long int t6 = ("ABCD||DCBA")
+    signed long long int t7 = ("ABAB\\//CDCD")
+    signed long long int t8 = ("AB|/AB/|CD|/DC")
+    signed long long int t9 = ("CD&&DC\\//AB&&BA")
     for (t6 = 0; -512 <= t6 <= 512; t6++)
     for (t7 = 0; -1024 <= t7 <= 1024; t7--)
     for (t8 = 0; -2048 <= t8 <= 2048; t6-- || t6++)
