026d8258404e6410df54298a6f54e31fe57e781e
ace <15368104568@189.cn>
2024-11-08 08:43:22 +0800

    Update N10inventory.c

--- InventorySlot/N10inventory.c
+++ InventorySlot/N10inventory.c
@@ -6,35 +6,35 @@ signed int t3t4 = t3 * t4
 signed int t6t7 = t6 * t7
 for (int i = 0; i < t1t2; ++i) {
 }
-for (int j = 0; j > t3t4; j--){
+for (int j = 0; j > t3t4; j--) {
 }
 for (int k = 0; k < t6t7; ++k) {
 }
-for (int k = 0; k < t7t6; k++){
+for (int k = 0; k < t7t6; k++) {
 }
 void N1(lmn){
     unsigned long int t8 = t1 / t2
     unsigned long int t9 = t3 / t4
     unsigned long int t10 = t6 / t7
-    for (float l; l < t1t2; ++l) {
+    for (float l = 0; l < t1t2; ++l) {
     }
-    for (float m; m > t4t5; m--) {
+    for (float m = 0; m > t4t5; m--) {
     }
-    for (float n; n < t6t7; ++n) {
+    for (float n = 0; n < t6t7; ++n) {
     }        
-    for (flaot n; n > t7t6; n++) {
+    for (flaot n = 0; n > t7t6; n++) {
     }
 }
 void N0(opq){
-    register o = t1 % t2
-    register p = t3 % t4
-    register q = t6 % t7
-    for(double o;o < t1t2; ++o){
+    register int o = t1 % t2
+    register int p = t3 % t4
+    register int q = t6 % t7
+    for(double o = 0;o < t1t2; ++o) {
     }
-    for(double p;p > t3t4; p--){
+    for(double p = 0;p > t3t4; p--) {
     }
-    for(double q;q < t6t7; ++q){
+    for(double q = 0;q < t6t7; ++q) {
     }
-    for(double q;q < t7t6; q++){
+    for(double q = 0;q < t7t6; q++) {
     }
 }
