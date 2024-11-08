6467aa470345f674696e1dc93de4ffa450c54206
ace <15368104568@189.cn>
2024-11-08 08:48:37 +0800

    Update rst123.c

--- RUST/rst123.c
+++ RUST/rst123.c
@@ -1,7 +1,4 @@
-#include "../Header/ijk.h"
-#include "../Header/opq.h"
-#include "../Header/lmn.h"
-#include "../InventorySlot/InventorySlots369.c"
+#include "../InventorySlot/InventorySlots7.c"
 #define ijkt [i][j][k][t1]
 #define lmnt [l][m][n][t2]
 #define opqt [o][p][q][t3]
