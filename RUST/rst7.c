8d454b637b7266403dc7ac3d748f29827a009ce0
unknownsaying <15368104568@189.cn>
2025-06-30 11:51:15 +0100

    Update rst7.c

--- RUST/rst7.c
+++ RUST/rst7.c
@@ -4,19 +4,19 @@ typedef struct ijk(){
         int i
         float j
         double k
-        double inline t7 <-> i <-> j <-> k <-> t7
+        inline t7 <-> i <-> j <-> k <-> t7
     }
 typedef struct opq(){
         int o
         float p
         double q
-        double inline t7 <-> o <-> p <-> q <-> t7
+        inline t7 <-> o <-> p <-> q <-> t7
         }
 typedef struct lmn(){
         int l
         float m
         double n
-        double inline t7 <-> l <-> m <-> n <-> t7
+        inline t7 <-> l <-> m <-> n <-> t7
     }
 }
 
